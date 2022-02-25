332664e3ffe8402451d03733ec0cc0a2639560a3
kimyoosang <yoo99485@gmail.com>
2022-02-25 19:49:54 +0900

    [Add] AboutPage

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <main>
+      <h1>About</h1>
+    </main>
+  );
+}
+
+export default AboutPage;
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,20 @@ import React from "react";
 
 function HomePage() {
   return (
-    <main>
-      <h1>My Blog</h1>
-    </main>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="/about">About</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <h1>My Blog</h1>
+      </main>
+    </>
   );
 }
 
