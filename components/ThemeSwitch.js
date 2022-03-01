fc8ad3665dd01017950cb32b28965c2f4f9bb664
kimyoosang <yoo99485@gmail.com>
2022-03-01 19:20:45 +0900

    [Update] suppresHydrationWarnning 주석

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -20,6 +20,9 @@ function ThemeSwitch() {
   const text = darkMode ? 'Light Mode' : 'Dark Mode';
   return (
     <>
+      {/* suppressHydrationWarning을 true로 설정하면, React는 어트리뷰트와 그
+      엘리먼트 내용의 불일치에 대해 경고하지 않습니다. 바로 밑 한 단계 깊이를
+      기준으로만 작동하며 해결책으로 사용하도록 되어 있습니다. */}
       <button onClick={handleClick} suppressHydrationWarning>
         {text}
       </button>
